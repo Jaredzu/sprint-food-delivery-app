1fc69fc1d5cc1ec3b75bac80bbb14b92593ab46f
Jaredzu <jared.zu96@gmail.com>
2023-02-07 23:05:38 -0500

    feat: Proyect Title

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RouterProvider } from 'react-router';
 import { router } from './Router';
 
+
 const App = () => {
+
+  useEffect(() => {
+    document.title = "Food Delivery App Sprint Jared"
+  }, []);
+
   return (
     <>
       <RouterProvider router={router}/>
