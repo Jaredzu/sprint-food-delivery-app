bcfdc6ec83237e923fc3792ad785b1ae25e420fa
Jaredzu <jared.zu96@gmail.com>
2023-02-10 22:58:41 -0500

    feat: add boostrap

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -4,33 +4,42 @@ import { Link, Outlet } from 'react-router-dom'
 const Navbar = () => {
     return (
         <>
-            <h1>
+            <h1 className='m-1'>
                 Navbar
             </h1>
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link className="nav-link" to="/" >
-                        Home
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/search" >
-                        Search
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/orders" >
-                        Orders
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <Link className="nav-link" to="/profile" >
-                        Profile
-                    </Link>
-                </li>
+            <nav className="navbar navbar-expand-sm bg-light">
 
-            </ul>
-            <Outlet />
+                <ul className="navbar-nav d-flex flex-row ">
+                    <li className="nav-item px-3">
+                        <Link className="nav-link" to="/" >
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item px-3">
+                        <Link className="nav-link" to="/search" >
+                            Search
+                        </Link>
+                    </li>
+                    <li className="nav-item px-3">
+                        <Link className="nav-link" to="/orders" >
+                            Orders
+                        </Link>
+                    </li>
+                    <li className="nav-item px-3">
+                        <Link className="nav-link" to="/profile" >
+                            Profile
+                        </Link>
+                    </li>
+
+                </ul>
+
+            </nav>
+
+            <div className="container p-2">
+
+                <Outlet />
+
+            </div>
         </>
     )
 }
