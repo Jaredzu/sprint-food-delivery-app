ab47efb1ca8bf30247c67d96324fbdb7839efcc0
Jaredzu <jared.zu96@gmail.com>
2023-02-11 17:29:49 -0500

    feat: add sass and Nav styles

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,33 +1,37 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import '../styles/Navbar.sass'
+
 
 const Navbar = () => {
     return (
         <>
-            <h1 className='m-1'>
-                Navbar
-            </h1>
+            <div className="container p-2">
+
+                <Outlet />
+
+            </div>
             <nav className="navbar navbar-expand-sm bg-light">
 
-                <ul className="navbar-nav d-flex flex-row ">
+                <ul className="navbar-nav d-flex flex-row justify-content-around w-100">
                     <li className="nav-item px-3">
                         <Link className="nav-link" to="/" >
-                            Home
+                            <i className="fa-solid fa-house-chimney"></i>
                         </Link>
                     </li>
                     <li className="nav-item px-3">
                         <Link className="nav-link" to="/search" >
-                            Search
+                        <i className="fa-solid fa-magnifying-glass"></i>
                         </Link>
                     </li>
                     <li className="nav-item px-3">
                         <Link className="nav-link" to="/orders" >
-                            Orders
+                        <i className="fa-solid fa-clock-rotate-left"></i>
                         </Link>
                     </li>
                     <li className="nav-item px-3">
                         <Link className="nav-link" to="/profile" >
-                            Profile
+                        <i className="fa-regular fa-user"></i>
                         </Link>
                     </li>
 
@@ -35,11 +39,7 @@ const Navbar = () => {
 
             </nav>
 
-            <div className="container p-2">
 
-                <Outlet />
-
-            </div>
         </>
     )
 }
