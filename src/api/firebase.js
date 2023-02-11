8c35734d2008484f164f051be546c482128ddd9b
Jaredzu <jared.zu96@gmail.com>
2023-02-10 23:35:12 -0500

    feat: added firebase & deleted test files

---
+++ src/api/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCCe4AOkeDn59yq9nz5V6gcsJKgYaW_djo",
+  authDomain: "sprint-food-delivery.firebaseapp.com",
+  projectId: "sprint-food-delivery",
+  storageBucket: "sprint-food-delivery.appspot.com",
+  messagingSenderId: "284184934604",
+  appId: "1:284184934604:web:6132345639c39e8bc9aa69"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
