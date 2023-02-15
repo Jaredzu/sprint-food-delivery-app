526db0f8c1d4eafaec007cde47a31ea454a27fce
Jaredzu <jared.zu96@gmail.com>
2023-02-15 03:14:27 -0500

    feat: add getRestaurants from Firecloud Function

--- src/api/restaurants.js
+++ src/api/restaurants.js
@@ -2,14 +2,29 @@ import {
 
     collection,
     addDoc,
+    getDocs
 
 } from "firebase/firestore";
 
 import { db } from "./firebase"
 
-// CRUD USERS
+// CRUD RESTAURANTS
 
-const dbCollection = collection(db, "Restaurants") // CAMBIAR NOMBREEEE
+const dbCollection = collection(db, "restaurants") 
 
+export const getRestaurants = async () => {
+    const firebaseRestaurants = await getDocs(dbCollection)
+    
+    const restaurants = []
+    
+  firebaseRestaurants.docs.forEach(d => {
+    const rest = d.data()
+    rest.id = d.id
+    restaurants.push(rest)
+      /*     data.push(doc.data({...user, id})) */
+  })
+    console.log(restaurants, "GET RESTAURANTS");
+  return { restaurants }
+}
 
 export const createRestaurant = async (restaurant) => await addDoc(dbCollection, restaurant)
