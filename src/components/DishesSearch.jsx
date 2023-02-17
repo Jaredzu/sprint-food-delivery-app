ec56fdd908c5c61c633f39a0ee29caffd74cc10b
Jaredzu <jared.zu96@gmail.com>
2023-02-17 12:42:52 -0500

    feat: add search layout

---
+++ src/components/DishesSearch.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const DishesSearch = ({}) => {
+  return (
+      <>
+            <div className='restaurants-section mb-3'>
+                        <div className="restaurant d-flex mb-2 align-items-center">
+                            <img className='mx-1 rounded-3' src="https://res.cloudinary.com/dif29zscp/image/upload/c_scale,h_104,w_132/v1676434717/food-app-delivery-s4/Restaurants/dishes/Le_Coin_burger_cr.33_lftefy.jpg" width={"18%"} alt="dish reference" />
+                            <div className="restaurant-info mx-2">
+                                <h6 className='restaurant-info-title mb-0'>name</h6>
+                                <p className='restaurant-info-schedule mb-0 text-body-tertiary'>Price: $ AAAAAAAAAAAA</p>
+                            </div>
+                        </div>
+                    </div>
+          
+    </>
+  )
+}
+
+export default DishesSearch
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import DishesSearch from '../components/DishesSearch';
+import "../styles/Search.sass"
 
 const Search = () => {
+
+  const handleSearch = (search) => {
+    console.log("Searching...",search);
+  }
+
   return (
-    <div>Search</div>
+    <>
+      <div className='searchContainer'>
+        <div className='glass'>
+          <i className='fa-solid fa-magnifying-glass'></i>
+        </div>
+        <input className='form-control rounded-3'
+          type='text'
+          placeholder="Search for Burger, Pizza, Pasta or Salad..."
+          onChange={event => handleSearch(event.target.value)}
+        />
+      </div>
+      <DishesSearch />
+    </>
   )
 }
 
